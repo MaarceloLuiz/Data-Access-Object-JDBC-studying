5b2787e87fb49f37c1fe39e1cdf25cdba67686ea
MaarceloLuiz <marceloluizgpereira@gmail.com>
2023-08-24 21:43:44 +0100

    simple data insertion using preparedStatement

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,52 +1,63 @@
 package application;
 
 import java.sql.Connection;
-import java.sql.ResultSet;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import db.DB;
-
 
 public class Program {
 
 	public static void main(String[] args) {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		Connection conn = null;
-		Statement st = null;
-		ResultSet rs = null;
-		
+		PreparedStatement st = null;
 		try {
 			conn = DB.getConnection();
-			//instanciar um objeto do tipo 'Statement'
-			st = conn.createStatement();
 			
-			//metodo executeQuery, que utiliza como argumento uma String SQL
-			rs = st.executeQuery("select * from department");
+			//? = place holder
+			//we will insert values on it
+			st = conn.prepareStatement("INSERT INTO seller "
+					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+					+ "VALUES "
+					+ "(?, ?, ?, ?, ?)");
 			
-			//while (is true). Because the 'next' returns false when there's no value left
-			while(rs.next()) {
-				//getting the 'Int' in the field "Id"
-				//getting the 'String' in the field "Name"
-				System.out.println(rs.getInt("Id") + " - " + rs.getString("Name"));
-			}
+			//replacing the first '?' by a String (Name)
+			st.setString(1, "Carl Purple");
+			//replacing the second '?' by a String (Email)
+			st.setString(2, "carl@gmail.com");
+			//replacing the third '?' by a Date (BirthDate)
+			//we have to use 'java.sql.Date()'
+			st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
+			//replacing the forth '?' by a Double (BaseSalary)
+			st.setDouble(4, 3000.0);
+			//replacing the fifth '?' by a Integer (Email)
+			st.setInt(5, 4);
+			
+			//we are updating the data, so we have to use 'executeUpdate()'
+			//the result is an Integer number depending on how many line were changed 
+			int rowsAffected = st.executeUpdate();
+			
+			System.out.println("Done! Rows affected: " + rowsAffected);
 		}
 		catch(SQLException e) {
 			e.printStackTrace();
 		}
-		//our resources 'Statement', 'ResultSet' they are external resources, so we should close manually all of them to prevent our program to have any 'memory leak' issues
+		//parseException otherwise the Date parse won't work
+		catch(ParseException e) {
+			e.printStackTrace();
+		}
+		//closing our resources
+		//always close connection last
 		finally {
-			/*
-			 * 
-			//to prevent using try catch all the time, we decided to create two new static methods in our DB class, so we can close properly our resources.
-			rs.close();
-			st.close();
-			*/
-			DB.closeResultSet(rs);
 			DB.closeStatement(st);
 			DB.closeConnection();
+			
+			//to check if it worked, we should go to mySQL Workbench and check the 'seller' tables
 		}
 
 	}
-
 }
