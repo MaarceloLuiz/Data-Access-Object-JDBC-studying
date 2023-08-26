d899c31833f3722e3760ef77943beff81069b74b
MaarceloLuiz <marceloluizgpereira@gmail.com>
2023-08-26 16:03:55 +0100

    deleting data && DbIntegrityException

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import db.DB;
+import db.DbIntegrityException;
 
 public class Program {
 
@@ -18,22 +19,24 @@ public class Program {
 			
 			//we still have to set values for our "?"
 			st = conn.prepareStatement(
-					"UPDATE seller "
-					+ "SET BaseSalary = BaseSalary + ? "
+					"DELETE FROM seller "
+					//'WHERE' is going to be our restriction, otherwise we will delete (or update) everyone.
 					+ "WHERE "
-					+ "(DepartmentId = ?)");
+					+ "Id = ?");
 			
-			//first '?'
-			st.setDouble(1, 200.0);
-			//second '?'
-			st.setInt(2, 2);
+			//if we tried, for example, to delete the department number 2, the program is going to give us an error.
+			//because there's sellers working on that department.
+			//we cannot delete a department with sellers linked to it.
+			
+			//first '?' - value (Id) = 8
+			st.setInt(1, 8);
 			
 			int rowsAffected = st.executeUpdate();
 			
 			System.out.println("Done! Rows affected: " + rowsAffected);
 		}
 		catch(SQLException e) {
-			e.printStackTrace();
+			throw new DbIntegrityException(e.getMessage());
 		}
 		//closing the resources
 		finally {
